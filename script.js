092f60d4afa0bf97ee04fc3571b3273521ef57dc
Ali Stoica Kovacs <78688064+alistoicakovacs@users.noreply.github.com>
2022-04-16 23:25:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ const rockButton = document.querySelector(".rock-button");
 const paperButton = document.querySelector(".paper-button");
 const scissorButton = document.querySelector(".scissor-button");
 const startButton = document.querySelector(".start-button");
+const endMessage = document.querySelector(".endMessage");
+const playAgainBtn = document.querySelector(".playAgain-button");
+
+const container = document.querySelector(".container");
 // With this event listener, the user input text is updated to which button is pressed
 userButtons.addEventListener("click", addUserInputText);
+
 // startButton.addEventListener("click", startFuntion);
 
 function addUserInputText(event) {
@@ -25,15 +30,47 @@ function addUserInputText(event) {
 
 	const choices = ["Rock", "Paper", "Scissor"];
 	const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-	computerInput.value = computerChoice;
 
-	if (userInput.value == computerInput.value) {
-		alert("Tie");
-	} else if (userInput.value > computerInput.value) {
-		alert("You won!");
-	} else alert("You Lost!");
+	startButton.addEventListener("click", function (e) {
+		computerInput.value = computerChoice;
+
+		// const text = "";
+
+		// switch (computerInput.value) {
+		// 	case "Rock":
+		// 		text = "You won";
+		// };
+		if (userInput.value === computerInput.value) {
+			endMessage.value = "It's a tie! Try again.";
+		} else if (
+			userInput.value === "Rock" &&
+			computerInput.value === "Scissor"
+		) {
+			endMessage.value = "You Won!";
+		} else if (userInput.value == "Rock" && computerInput.value == "Paper") {
+			endMessage.value = "You Lost! Try again!";
+		} else if (userInput.value == "Paper" && computerInput.value == "Scissor") {
+			endMessage.value = "You Lost! Try again!";
+		} else if (userInput.value == "Paper" && computerInput.value == "Rock") {
+			endMessage.value = "You Won!";
+		} else if (userInput.value == "Scissor" && computerInput.value == "Rock") {
+			endMessage.value = "You Lost! Try again!";
+		} else if (userInput.value == "Scissor" && computerInput.value == "Paper") {
+			endMessage.value = "You Won!";
+		}
+
+		endMessage.readOnly = true;
+		startButton.readOnly = true;
+
+		// if (endMessage.value == "You Won!") {
+		// 	container.classList.add("overlay");
+		// }
+	});
 }
 
+playAgainBtn.addEventListener("click", function () {
+	location.reload();
+});
 // function startFunction() {
 // 	console.log("click");
 // }
